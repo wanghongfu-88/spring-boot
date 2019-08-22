72b7b4fd1e2c53e7a27612309827173871370798
01585524 <01585524@yto.net.cn>
2019-08-22 10:18:14 +0800

    提交

---
+++ commons/src/main/java/com/walliam/App.java
@@ -0,0 +1,13 @@
+package com.walliam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
